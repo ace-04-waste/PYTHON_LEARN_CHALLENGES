b6fe37f78eb7cb2c581f37867cf01b785315779f
ace-04-waste <77788564+ace-04-waste@users.noreply.github.com>
2023-07-03 19:08:12 +0100

    Add files via upload

---
+++ elif_statements.py
@@ -0,0 +1,21 @@
+score = float(input('what is your nummber'))
+
+if score <0 or score >1:
+    print('error')
+    
+elif score >= 0.9:
+    print('A')
+
+elif score >= 0.8:
+    print('B')
+    
+elif score >= 0.7:
+    print('C')
+    
+elif score >= 0.6:
+    print('E')
+    
+else:
+    print('F')
+    
+    
---
+++ tuples.py
@@ -0,0 +1,16 @@
+x = ('1','2','3','4','5','6')
+print(type(x))
+
+y = ('a',)
+print(type(y))
+
+y = ('a')
+print(type(y))
+
+t1 = tuple('matrix')
+print(t1)
+print(t1[2])
+print(t1[1:3])
+
+t1 = ('1',) + t1[1:]
+print(t1)
